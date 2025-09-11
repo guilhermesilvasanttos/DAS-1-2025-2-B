b37fc70a52db063c9a4df0979a548efd874da486
guilhermesilvasanttos <127353872+guilhermesilvasanttos@users.noreply.github.com>
2025-09-11 23:11:27 +0000

    novo

---
+++ demo/src/main/java/eventos/Publisher.java
@@ -0,0 +1,12 @@
+package br.univille;
+
+public class Publisher {
+    public static void main (String[] args){
+        var topicName = "topic-das1-b";
+        var servidor = "https://sbdas12025a.servicebus.windows.net";
+        
+        DefaultAzureCredential credential =
+            new DefaultAzureCredentialBuilder().build();
+        
+    }
+}
