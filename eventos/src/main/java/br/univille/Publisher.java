74a0b967c6adcca6ea1c3d3a88fde9b5a270b44c
guilhermesilvasanttos <127353872+guilhermesilvasanttos@users.noreply.github.com>
2025-09-15 23:16:41 +0000

    novo novo

--- eventos/src/main/java/br/univille/Publisher.java
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -8,8 +8,23 @@ public class Publisher {
         var topicName = "topic-das1-b";
         var servidor = "https://sbdas12025a.servicebus.windows.net";
 
-        DefaultAzureCredential credential = 
-            new DefaultAzureCredentialBuilder().build();
+        //DefaultAzureCredential credential = 
+        //    new DefaultAzureCredentialBuilder().build();
+
+        String chave = System.getenv("CHAVE");
+
+        ServiceBusSenderClient senderClient =
+            new serviceBusClientBuilder()
+                .fullyQualifiedNamespace(servidor)
+                .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+                //.credential(credential)
+                .connectionString(chave)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+
+        senderClient.sendMessage(new ServiceBusMessage("Guilherme: Vai corinthians!"));
+        
 
     }
 
